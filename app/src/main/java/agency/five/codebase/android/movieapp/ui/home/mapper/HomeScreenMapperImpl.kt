7f0f074ded8965a20c36f76f9fcb630bcbdba00c
MVuko123 <vuko.marin52@gmail.com>
2022-11-15 19:11:35 +0100

    Napraviti jos HomeScreen za 3.zadatak

---
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/home/mapper/HomeScreenMapperImpl.kt
@@ -0,0 +1,49 @@
+package agency.five.codebase.android.movieapp.ui.home.mapper
+
+import agency.five.codebase.android.movieapp.R
+import agency.five.codebase.android.movieapp.model.Movie
+import agency.five.codebase.android.movieapp.model.MovieCategory
+import agency.five.codebase.android.movieapp.ui.component.MovieCategoryLabelViewState
+import agency.five.codebase.android.movieapp.ui.component.MovieCategoryStringRes
+import agency.five.codebase.android.movieapp.ui.home.HomeMovieCategoryViewState
+import agency.five.codebase.android.movieapp.ui.home.HomeMovieViewState
+
+class HomeScreenMapperImpl() : HomeScreenMapper {
+    override fun toHomeMovieCategoryViewState(
+        movieCategories: List<MovieCategory>,
+        selectedMovieCategory: MovieCategory,
+        movies: List<Movie>
+    ): HomeMovieCategoryViewState {
+       return HomeMovieCategoryViewState(movieCategories.map { movieCategory ->
+           MovieCategoryLabelViewState(
+               movieCategory.ordinal,
+               movieCategory == selectedMovieCategory,
+               MovieCategoryStringRes(
+                   getStringRes(movieCategory)
+               ),
+           )
+       },
+           movies.map { movie ->
+               HomeMovieViewState(
+                   movie.id,
+                   movie.isFavorite,
+                   movie.imageUrl
+               )
+           }
+       )
+    }
+
+    private fun getStringRes(movieCategory: MovieCategory): Int {
+        return when (movieCategory){
+            MovieCategory.POPULAR_STREAMING -> R.string.streaming
+            MovieCategory.POPULAR_ON_TV -> R.string.on_tv
+            MovieCategory.POPULAR_FOR_RENT -> R.string.for_rent
+            MovieCategory.POPULAR_IN_THEATRES -> R.string.in_theatres
+            MovieCategory.NOW_PLAYING_MOVIES -> R.string.movies
+            MovieCategory.NOW_PLAYING_TV -> R.string.tv
+            MovieCategory.UPCOMING_TODAY -> R.string.today
+            MovieCategory.UPCOMING_THIS_WEEK -> R.string.this_week
+        }
+
+    }
+}
